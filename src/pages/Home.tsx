b942fc23599d11e357d7b5a158b2f777bb2b3fe9
bernardobfg <64651224+bernardobfg@users.noreply.github.com>
2021-06-22 15:00:38 -0300

    altera context da Home pra hooks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useContext } from "react";
 import { useHistory } from "react-router-dom"
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
@@ -7,10 +6,10 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss"
 import { Button } from "../components/Button";
 
-import { AuthContext } from "../contexts/AuthContext";
+import {useAuth} from "../hooks/useAuth"
 export function Home() {
     const history = useHistory();
-    const {user, signInWithGoogle} = useContext(AuthContext)
+    const {user, signInWithGoogle} = useAuth()
 
     async function handleCreateRoom() {
         if (!user) {
